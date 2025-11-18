edf89c2151de46ebd07ec27064001a34c9061547
david.escue@gmail.com <david.escue@gmail.com>
2025-11-18 14:59:11 -0800

    initial layout

--- 02-tic-tac-toe/src/App.tsx
+++ 02-tic-tac-toe/src/App.tsx
@@ -1,5 +1,28 @@
 function App() {
-  return <h1>React Tic-Tac-Toe</h1>;
+  return (
+    <>
+      <header>
+        <img src='game-logo.png' alt='Hand-drawn tic tac toe game board' />
+        <h1>Tic-Tac-Toe</h1>
+      </header>
+      <main>
+        <div id='game-container'>
+          <ol id='players'>
+            <li>
+              <span className='player-name'>Player 1</span>
+              <span className='player-sympol'>X</span>
+            </li>
+            <li>
+              <span className='player-name'>Player 2</span>
+              <span className='player-sympol'>O</span>
+            </li>
+          </ol>
+          GAME BOARD
+        </div>
+        LOG
+      </main>
+    </>
+  );
 }
 
 export default App;
